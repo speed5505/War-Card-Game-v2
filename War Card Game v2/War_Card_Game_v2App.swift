ccb7d5cc6d999fdc6af035019c5c2cdb2c4b0e55
Kabir Rajkotia <kabir.rajkotia@gmail.com>
2024-08-05 21:59:55 +0530

    Initial Commit

---
+++ War Card Game v2/War_Card_Game_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Game_v2App.swift
+//  War Card Game v2
+//
+//  Created by Kabir Rajkotia on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Game_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
