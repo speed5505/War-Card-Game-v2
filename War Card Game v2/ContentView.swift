5e1c118af2fc69fbe97a9294b6b5358f99b3f20c
Kabir Rajkotia <kabir.rajkotia@gmail.com>
2024-08-06 17:10:36 +0530

    Button and deal function

--- War Card Game v2/ContentView.swift
+++ War Card Game v2/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        var playerCard = "card7"
+        var cpuCard = "card13"
+        var playerScore = 0
+        var cpuScore = 0
+        
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                Image("logo")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    deal()
+                }, label: {
+                    Image("button")
+                })
+                 
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding(15)
+    }
+    
+    
+    func deal() {
+        print("deal cards")
+        
     }
 }
 
